242b7510ccc040a6f8c4988e5e5745e6665cf789
JUSTMEETPATEL <justmeetpatel@gmail.com>
2026-02-18 12:09:40 +0530

    feat: created website for Flash

--- docs/website/src/app/layout.tsx
+++ docs/website/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
           >
             {children}
           </ThemeProvider>
+          <Analytics />
       </body>
     </html>
   );
